3ed5e2e15e13fd29f6fe17ce1e13424e715999f2
sitaochen <sailer_cst@yahoo.com>
2019-03-26 18:17:52 -0400

    added script for 7000 rows

--- sample_data.py
+++ sample_data.py
@@ -1,11 +1,11 @@
 from iex import Stock
+from pandas import DataFrame
 
 
 tgt_stock_label = "AAPL"
-raw_data = Stock(tgt_stock_label).chart_table("1d")
-
-x = raw_data.loc[:, "changeOverTime":"volume"]
-y = raw_data["average"]
-
-print(x)
-print(y)
+raw_data = DataFrame()
+date = "20190326"
+while len(raw_data) < 50000 and int(date) > 20190300:
+	raw_data = raw_data.append(Stock(tgt_stock_label).chart_table(date), ignore_index=True)
+	date = str(int(date) - 1)
+print(raw_data)
