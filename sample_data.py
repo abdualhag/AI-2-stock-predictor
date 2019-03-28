e8799ed57f491b4a4384d9f9787cd53866fbc6b0
sitaochen <sailer_cst@yahoo.com>
2019-03-27 22:43:56 -0400

    cleaned data

--- sample_data.py
+++ sample_data.py
@@ -9,3 +9,6 @@ while len(raw_data) < 50000 and int(date) > 20190300:
 	raw_data = raw_data.append(Stock(tgt_stock_label).chart_table(date), ignore_index=True)
 	date = str(int(date) - 1)
 print(raw_data)
+raw_data = raw_data[raw_data["average"] != -1]
+x = raw_data.loc[:, "changeOverTime": "volumn"]
+y = raw_data["average"]
